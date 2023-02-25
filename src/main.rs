b70eca21e94c2ce4e6ee03436f9bbcaf3da12369
breadyboi <95249362+NockKnock@users.noreply.github.com>
2023-02-24 19:53:49 -0500

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 fn main() {
     loop {
         let user_sentence: String = get_user_sentence();
-        println!("You input: {}", user_sentence);
+        println!("You input: {user_sentence}");
         println!(
             "There are {} vowels in your sentence.",
             vowel_counter(&user_sentence)
@@ -19,10 +19,10 @@ fn get_user_sentence() -> String {
     io::stdin()
         .read_line(&mut user_sentence)
         .expect("Could not read sentence");
-    return user_sentence;
+    user_sentence
 }
 
-fn vowel_counter(user_sentence: &String) -> i32 {
+fn vowel_counter(user_sentence: &str) -> i32 {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     user_sentence
         .trim()
